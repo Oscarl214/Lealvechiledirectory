408a1204e3597cf78d73a695aa4ec584bdca6fbd
Oscar Leal <oscarleal234@gmail.com>
2024-06-07 16:07:06 -0500

    added better UI to SIgnUp page

--- app/page.js
+++ app/page.js
@@ -1,48 +1,116 @@
-'use client'
-import React from "react";
-import signIn from "../app/firebase/auth/signin";
-import { useRouter } from 'next/navigation'
+'use client';
+import React from 'react';
+import signIn from '../app/firebase/auth/signin';
+import { useRouter } from 'next/navigation';
+import { Image, Button } from '@nextui-org/react';
 
 function SignInPage() {
-    const [email, setEmail] = React.useState('')
-    const [password, setPassword] = React.useState('')
-    const router = useRouter()
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const router = useRouter();
 
-    const handleForm = async (event) => {
-        event.preventDefault()
+  const handleForm = async (event) => {
+    event.preventDefault();
 
-        const { result, error } = await signIn(email, password);
+    const { result, error } = await signIn(email, password);
 
-        if (error) {
-            return console.log(error)
-        }
-
-        // else successful
-        console.log(result)
-        return router.push("/admin")
+    if (error) {
+      return console.log(error);
     }
-    return (
-    
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="wrapper">
-        <div className="form-wrapper">
-            <h1 className="mt-60 mb-30">Sign up</h1>
-            <form onSubmit={handleForm} className="form">
-                <label htmlFor="email">
-                    <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
-                </label>
-                <label htmlFor="password">
-                    <p>Password</p>
-                    <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
-                </label>
-                <button type="submit">Sign up</button>
-            </form>
-        </div>
 
-    </div>
-    
-    </main>);
+    // else successful
+    console.log(result);
+    return router.push('/admin');
+  };
+  return (
+    <main className="flex min-h-screen flex-col lg:flex-row items-center justify-around lg:m-[5rem]">
+      <div>
+        {' '}
+        <Image
+          isZoomed
+          isBlurred
+          width={550}
+          height={500}
+          src="https://firebasestorage.googleapis.com/v0/b/lealvehicledirectory.appspot.com/o/SignUpPageGTR.jpg?alt=media&token=0bd51fcd-b6ab-4018-85ca-ce1e660b6dc4"
+          alt="Nissan GTR"
+          className="m-5"
+        />
+      </div>
+      <section class="">
+        <div class="flex flex-col items-center justify-center  lg:py-0">
+          <a
+            href="#"
+            class="flex items-center mb-6 text-2xl font-semibold text-5xl"
+          >
+            {/* <img
+              class="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              alt="logo"
+            /> */}
+            Sign Up
+          </a>
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create an account
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@gmail.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+
+                <Button
+                  color="primary"
+                  variant="bordered"
+                  className="text-white"
+                >
+                  Bordered
+                </Button>
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{' '}
+                  <a
+                    href="#"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Login here
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default SignInPage;
