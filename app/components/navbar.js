4f7a63135024ba0d2a166457af2b0c316d0989a6
Oscar Leal <oscarleal234@gmail.com>
2024-06-11 22:23:53 -0500

    added animation

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,50 +1,56 @@
-'use client'
-import React from 'react'
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button, Image} from "@nextui-org/react";
-import  {useAuthContext} from '../context/AuthContext'
+'use client';
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+  Image,
+} from '@nextui-org/react';
+import { useAuthContext } from '../context/AuthContext';
 
 import logOut from '../firebase/auth/logout';
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const handleLogOut = async (event) => {
+    event.preventDefault();
 
-    const handleLogOut = async (event) => {
-        event.preventDefault();
-    
-        const { result, error } = await logOut();
-    
-        if (error) {
-          return console.log('Error logging user out', error);
-        }
-        console.log(result);
-        return Router.push('/');
-      };
+    const { result, error } = await logOut();
 
-    const menuItems = [
-      "Profile",
-      "Dashboard",
-      "Log Out",
-    ];
+    if (error) {
+      return console.log('Error logging user out', error);
+    }
+    console.log(result);
+    return Router.push('/');
+  };
+
+  const menuItems = ['Profile', 'Dashboard', 'Log Out'];
 
   return (
-    <div>   <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-trasnparent font-popping">
-    <NavbarContent>
-      <NavbarMenuToggle
-        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-        className="sm:hidden"
-      />
-      <NavbarBrand>
-      <Image
-      width={50}
-      alt="NextUI hero Image"
-      src="./favicon.png"
-    />
-        
-      </NavbarBrand>
-    </NavbarContent>
+    <div>
+      {' '}
+      <Navbar
+        onMenuOpenChange={setIsMenuOpen}
+        className="bg-gray-400  font-popping"
+      >
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <Image width={50} alt="NextUI hero Image" src="./favicon.png" />
+          </NavbarBrand>
+        </NavbarContent>
 
-    {/* <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {/* <NavbarContent className="hidden sm:flex gap-4" justify="center">
       <NavbarItem>
         <Link color="foreground" href="#">
           Features
@@ -61,41 +67,45 @@ const NavBar = () => {
         </Link>
       </NavbarItem>
     </NavbarContent> */}
-    <NavbarContent justify="end">
-      {/* <NavbarItem className="hidden lg:flex">
+        <NavbarContent justify="end">
+          {/* <NavbarItem className="hidden lg:flex">
         <Link href="#">Login</Link>
       </NavbarItem> */}
-      <NavbarItem>
-   
-        <Button onClick={handleLogOut} color="primary" href="#" variant="flat" className='bg-trasnparent text-gray-300'>
-          Log Out
-        </Button>
-      </NavbarItem>
-    </NavbarContent>
-    <NavbarMenu>
-      {menuItems.map((item, index) => (
-        <NavbarMenuItem key={`${item}-${index}`}>
-          <Link
-            color={
-              index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-            }
-            className="w-full"
-            href="#"
-            size="lg"
-          >
-            {item}
-          </Link>
-        </NavbarMenuItem>
-      ))}
-    </NavbarMenu>
-  </Navbar></div>
-  )
-}
+          <NavbarItem>
+            <Button
+              onClick={handleLogOut}
+              color="primary"
+              href="#"
+              variant="flat"
+              className="bg-transparent text-black"
+            >
+              Log Out
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu>
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? 'primary'
+                    : index === menuItems.length - 1
+                    ? 'danger'
+                    : 'foreground'
+                }
+                className="w-full"
+                href="#"
+                size="lg"
+              >
+                {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+    </div>
+  );
+};
 
-export default NavBar
-
-
-
-
-
-
+export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,25 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    animation: {
+      blob: 'blob 7s infinite',
+    },
+    keyframes: {
+      blob: {
+        '0%': {
+          transform: ' translate(0px,0px) scale(1)',
+        },
+        '33%': {
+          transform: ' translate(20px,-20px) scale(1.1)',
+        },
+        '66%': {
+          transform: 'translate(-20px,20px) scale(0.9)',
+        },
+        '100%': {
+          transform: 'translate(0px,0px) scale(1)',
+        },
+      },
+    },
   },
   darkMode: 'class',
   plugins: [
