d65408bffcd0422283931043afa8cf69470ae17b
Oscar Leal <oscarleal234@gmail.com>
2024-06-16 13:39:59 -0500

    touches to ui

--- app/components/CarCard.tsx
+++ app/components/CarCard.tsx
@@ -9,7 +9,7 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
   return (
-    <div className="flex flex-col p-6 justify-center items-start text-black-100 bg-primary-blue-100 hover:bg-white hover:shadow-md rounded-3xl">
+    <div className="flex flex-col p-6 justify-center items-start text-black-100 bg-primary-blue-100 hover:bg-white hover:shadow-md rounded-3xl w-[500px]">
       <div className="w-full flex flex-col justify-between items-start gap-2">
         <h2 className="text-[22px] leading-[26px] font-bold capitalize text-black">
           {make} {model}
@@ -27,6 +27,39 @@ const CarCard = ({ car }: CarCardProps) => {
             priority
             className="object-contain"
           />
+        </div>
+        <div className="relative flex w-full mt-2">
+          <div className="flex group-hover:invisible w-full justify-between text-gray">
+            <div className="flex flex-col justify-center items-center gap-2">
+              <Image
+                src="/steering-wheel.svg"
+                alt="steering wheel"
+                width={20}
+                height={20}
+              />
+              <p className="text-[14px] text-gray-400">
+                {transmission === 'a' ? 'Automatic' : 'Manual'}
+              </p>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-2">
+              <Image
+                src="/tire.svg"
+                alt="steering wheel"
+                width={20}
+                height={20}
+              />
+              <p className="text-[14px] text-gray-400">{drive.toUpperCase()}</p>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-2">
+              <Image
+                src="/gas.svg"
+                alt="steering wheel"
+                width={20}
+                height={20}
+              />
+              <p className="text-[14px] text-gray-400">{city_mpg} MPG</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
