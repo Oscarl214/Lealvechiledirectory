c0e9243f73b205409a335ce9df617e4cb2bdd044
Oscar Leal <oscarleal234@gmail.com>
2024-06-20 21:47:07 -0500

    messing with cache

--- app/api/route.js
+++ app/api/route.js
@@ -1,78 +1,45 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
+const express = require('express');
+const app = express();
 
-// const vehicles= [
-//     {
-//         id: 1,
-//         make: "Chevrolet",
-//         model: "Silverado",
-//         year: 2021,
-//         fuel_type: "gas",
-//         drive: "rwd",
-//         transmission: "a",
-//         cylinders: 8,
-//         displacement: 5.3,
-//         city_mpg: 17,
-//         highway_mpg: 23,
-//         combination_mpg: 19,
-//         class: "pickup"
-//     },
-//     {
-//         id: 2,
-//         make: "Toyota",
-//         model: "4Runner",
-//         year: 2019,
-//         fuel_type: "gas",
-//         drive: "4wd",
-//         transmission: "a",
-//         cylinders: 6,
-//         displacement: 4.0,
-//         city_mpg: 17,
-//         highway_mpg: 20,
-//         combination_mpg: 18,
-//         class: "SUV"
-//     },
-// ];
+// Middleware to set no-cache headers for all responses
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  next();
+});
 
-// export default function get(req: NextApiRequest, res: NextApiResponse) {
-//     // Respond with JSON data
-//     res.status(200).json(vehicles);
-// }
+export async function GET() {
+  let res = [
+    {
+      id: 1,
+      make: 'Chevrolet',
+      model: 'Silverado',
+      year: 2021,
+      fuel_type: 'gas',
+      drive: 'rwd',
+      transmission: 'a',
+      cylinders: 8,
+      displacement: 5.3,
+      city_mpg: 17,
+      highway_mpg: 23,
+      combination_mpg: 19,
+      class: 'pickup',
+    },
+    {
+      id: 2,
+      make: 'Toyota',
+      model: '4Runner',
+      year: 2019,
+      fuel_type: 'gas',
+      drive: '4wd',
+      transmission: 'a',
+      cylinders: 6,
+      displacement: 4.0,
+      city_mpg: 17,
+      highway_mpg: 20,
+      combination_mpg: 18,
+      class: 'SUV',
+    },
+  ];
 
-
-export async function GET(){
-    let res=[
-        {
-            id: 1,
-            make: "Chevrolet",
-            model: "Silverado",
-            year: 2021,
-            fuel_type: "gas",
-            drive: "rwd",
-            transmission: "a",
-            cylinders: 8,
-            displacement: 5.3,
-            city_mpg: 17,
-            highway_mpg: 23,
-            combination_mpg: 19,
-            class: "pickup"
-        },
-        {
-            id: 2,
-            make: "Toyota",
-            model: "4Runner",
-            year: 2019,
-            fuel_type: "gas",
-            drive: "4wd",
-            transmission: "a",
-            cylinders: 6,
-            displacement: 4.0,
-            city_mpg: 17,
-            highway_mpg: 20,
-            combination_mpg: 18,
-            class: "SUV"
-        },
-    ];
-
-    return Response.json(res)
-
+  return Response.json(res);
 }
--- app/components/CarCard.js
+++ app/components/CarCard.js
@@ -1,100 +1,96 @@
-
 import React from 'react';
 import Image from 'next/image';
-
-
+import { cookies } from 'next/headers';
 import { Button } from '@nextui-org/react';
 const CarCard = async () => {
+  const _cookies = cookies();
 
-const getVechicles= async ()=>{
-  
-  
-  const res = await fetch ('http://localhost:3000/api', {
-    cache: 'no-cache',
-  })
-  return res.json()
+  const getVehicles = async () => {
+    try {
+      const res = await fetch('http://localhost:3000/api', {
+        cache: 'force-cache',
+      });
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-}
+      console.log(res);
+      return res.json();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return []; // or handle error state
+    }
+  };
 
-const data= await getVechicles()
+  const data = await getVehicles();
 
-console.log(data)
+  console.log(data);
 
   return (
-  <div>
- {data.map((vechicle)=>(   
-  
-    <div className="flex flex-col p-6 justify-center items-start text-black-100 bg-gray-200 hover:bg-white hover:shadow-md rounded-3xl w-[500px] m-5" key={vechicle.id}>
-   
-
-      
-      <div className="w-full flex flex-col justify-between items-start gap-3" >
-        <h2 className="text-[22px] leading-[26px] font-bold capitalize text-black">
-         {vechicle.model}
-        </h2>
-        <p>
-          <span className="self-start text-[24px] font-semibold text-black">
-          
-          </span>
-        </p>
-        <div className="relative w-full h-40 my-3 object-contain">
-          <Image
-            src={'/carex.png'}
-            alt="car model"
-            fill
-            className="object-contain"
-          />
-        </div>
-        <div className="relative flex w-full mt-2">
-          <div className="flex group-hover:invisible w-full justify-between text-gray">
-            <div className="flex flex-col justify-center items-center gap-2">
+    <div>
+      {data.map((vechicle) => (
+        <div
+          className="flex flex-col p-6 justify-center items-start text-black-100 bg-gray-200 hover:bg-white hover:shadow-md rounded-3xl w-[500px] m-5"
+          key={vechicle.id}
+        >
+          <div className="w-full flex flex-col justify-between items-start gap-3">
+            <h2 className="text-[22px] leading-[26px] font-bold capitalize text-black">
+              {vechicle.model}
+            </h2>
+            <p>
+              <span className="self-start text-[24px] font-semibold text-black"></span>
+            </p>
+            <div className="relative w-full h-40 my-3 object-contain">
               <Image
-                src="/steering-wheel.svg"
-                alt="steering wheel"
-                width={20}
-                height={20}
+                src={'/carex.png'}
+                alt="car model"
+                fill
+                className="object-contain"
               />
-              {/* <p className="text-[14px] text-gray-400">
+            </div>
+            <div className="relative flex w-full mt-2">
+              <div className="flex group-hover:invisible w-full justify-between text-gray">
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <Image
+                    src="/steering-wheel.svg"
+                    alt="steering wheel"
+                    width={20}
+                    height={20}
+                  />
+                  {/* <p className="text-[14px] text-gray-400">
                 {transmission === 'a' ? 'Automatic' : 'Manual'}
               </p> */}
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <Image
+                    src="/tire.svg"
+                    alt="steering wheel"
+                    width={20}
+                    height={20}
+                  />
+                  <p className="text-[14px] text-gray-400"></p>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <Image
+                    src="/gas.svg"
+                    alt="steering wheel"
+                    width={20}
+                    height={20}
+                  />
+                  <p className="text-[14px] text-gray-400"> MPG</p>
+                </div>
+              </div>
+
+              <div className=" flex justify-center ml-4 pl-4">
+                <Button variant="ghost" color="primary" className="text-black">
+                  Add to Profile
+                </Button>
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <Image
-                src="/tire.svg"
-                alt="steering wheel"
-                width={20}
-                height={20}
-              />
-              <p className="text-[14px] text-gray-400"></p>
-            </div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <Image
-                src="/gas.svg"
-                alt="steering wheel"
-                width={20}
-                height={20}
-              />
-              <p className="text-[14px] text-gray-400"> MPG</p>
-            </div>
-            
           </div>
-          
-          <div className=" flex justify-center ml-4 pl-4">
-            <Button variant="ghost" color="primary" className="text-black">
-              Add to Profile
-            </Button>
-        
-          </div>
-          
         </div>
-       
-      
-      </div>
-   
+      ))}
     </div>
-     ))}
-    </div>
-   
   );
 };
 
